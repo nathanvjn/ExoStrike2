b1d4afc8d530acfc653b737c003eb65bcc4d0d0f
LusseveldNiek <97088465@st.deltion.nl>
2023-09-13 14:57:25 +0200

    movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,14 +5,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public float maxSpeed;
+    public float gravity;
+    private float normalDrag;
+    public float dragWhenPlayerNotMoving;
+    private void Start()
+    {
+        normalDrag = GetComponent<Rigidbody>().drag;
+    }
 
     void Update()
     {
+        //movement
         float forwardSpeed = Input.GetAxis("Vertical");
         GetComponent<Rigidbody>().AddForce(transform.forward * forwardSpeed * speed);
 
         float sideSpeed = Input.GetAxis("Horizontal");
         GetComponent<Rigidbody>().AddForce(transform.right * sideSpeed * speed);
-        
+
+        //add drag when player not moving
+        if(sideSpeed < 0.6f && forwardSpeed < 0.6f)
+        {
+            GetComponent<Rigidbody>().drag = dragWhenPlayerNotMoving;
+        }
+
+        else
+        {
+            GetComponent<Rigidbody>().drag = normalDrag;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //limit speed
+        if(GetComponent<Rigidbody>().velocity.magnitude > maxSpeed)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody>().velocity, maxSpeed);
+        }
+
+        //change gravity
+        GetComponent<Rigidbody>().AddForce(-transform.up * gravity);
     }
 }
