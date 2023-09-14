7c2ae871ac3cc33863e0b9817a7a3e0983a233e1
LusseveldNiek <97088465@st.deltion.nl>
2023-09-14 13:42:41 +0200

    sliding

--- Assets/Scripts/Player/PlayerSliding.cs
+++ Assets/Scripts/Player/PlayerSliding.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSliding : MonoBehaviour
 {
+
+    public Slider slider;
+
+    public float slidingTime;
+    private float beginningSlidingTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        beginningSlidingTime = slidingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        slider.value = slidingTime;
+        if(Input.GetKey(KeyCode.LeftShift) && slidingTime > 0)
+        {
+            //sliding
+            slidingTime -= Time.deltaTime;
+        }
+
+        else
+        {
+            //reset sliding time
+            if(slidingTime < beginningSlidingTime)
+            {
+                slidingTime += Time.deltaTime;
+            }
+
+            else
+            {
+                slidingTime = beginningSlidingTime;
+            }
+        }
     }
 }
