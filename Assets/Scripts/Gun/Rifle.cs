679cca8c1e3495274477e4c5d85e1ee0ddb15c3e
LusseveldNiek <97088465@st.deltion.nl>
2023-09-22 10:22:07 +0200

    raycast visible

--- Assets/Scripts/Gun/Rifle.cs
+++ Assets/Scripts/Gun/Rifle.cs
@@ -17,8 +17,8 @@ public class Rifle : MonoBehaviour
     void Update()
     {
         Physics.Raycast(cam.position, cam.forward, out hit, 100);
-
-        if(hit.transform != null)
+        Debug.DrawLine(cam.position, hit.point, Color.red);
+        if (hit.transform != null)
         {
             if (hit.transform.gameObject.tag == "Player" && Input.GetButtonDown("Fire1"))
             {
