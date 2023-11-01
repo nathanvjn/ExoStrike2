e0f79a611454b678d71440c19c9478d01b443789
LusseveldNiek <97088465@st.deltion.nl>
2023-11-01 15:35:22 +0100

    bounce material fix

--- Assets/Scripts/Gun/Prefab/Bullet/BouncyBullet.cs
+++ Assets/Scripts/Gun/Prefab/Bullet/BouncyBullet.cs
@@ -6,6 +6,14 @@ public class BouncyBullet : MonoBehaviour
 {
     public float bulletDamage;
     public float lifetime;
+
+    public Material noEmmisionMaterial;
+    public Material emmisionMaterial;
+
+    private bool activateEmmision;
+    private float emmisionCounter;
+    public float maxEmmisionCount;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -19,10 +27,28 @@ public class BouncyBullet : MonoBehaviour
             //ignore collision with other bullets
             Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
         }
+
+        else
+        {
+            activateEmmision = true;
+        }
     }
 
     private void Update()
     {
         Destroy(gameObject, lifetime);
+
+        //turn on emmision when bouncing on object
+        if(activateEmmision)
+        {
+            GetComponent<Renderer>().material = emmisionMaterial;
+            emmisionCounter += Time.deltaTime;
+            if(emmisionCounter >= maxEmmisionCount)
+            {
+                emmisionCounter = 0;
+                GetComponent<Renderer>().material = noEmmisionMaterial;
+                activateEmmision = false;
+            }
+        }
     }
 }
