af0050d5ab73335efb9fa4b223d0febafe753932
LusseveldNiek <97088465@st.deltion.nl>
2023-11-02 14:55:34 +0100

    sliding animation

--- Assets/Scripts/Player/PlayerSliding.cs
+++ Assets/Scripts/Player/PlayerSliding.cs
@@ -29,6 +29,9 @@ public class PlayerSliding : MonoBehaviour
     public float slideForce;
     public float maxSlideAngle;
 
+    //gun
+    public Animator gun;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,23 +41,24 @@ public class PlayerSliding : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-   
+
         //test
         velocity = r.velocity;
         magnitude = r.velocity.magnitude;
         //test
 
         //limit velocity when sliding and jumping
-        if(r.velocity.y > 30)
+        if (r.velocity.y > 30)
         {
             r.velocity = new Vector3(r.velocity.x, 30, r.velocity.z);
         }
 
         //sliding
-        if(Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             //sliding
             isSliding = true;
+            gun.SetBool("sliding", true);
 
             //sliding speed is the speed the player begins with when sliding
             GetComponent<PlayerMovement>().speed = slidingSpeed;
@@ -63,12 +67,12 @@ public class PlayerSliding : MonoBehaviour
             {
                 //cam position (sliding effect)
                 cam.position = camSlidingPosition.position;
-                
+
                 //slowing down
                 slidingSpeed -= amountOfSlowingDown;
             }
 
-            if(GetComponent<PlayerMovement>().isGrounded)
+            if (GetComponent<PlayerMovement>().isGrounded)
             {
                 RaycastHit hit;
                 Vector3 playerBottom = transform.position - new Vector3(0f, GetComponent<CapsuleCollider>().height / 2f - GetComponent<CapsuleCollider>().radius, 0f);
@@ -87,19 +91,25 @@ public class PlayerSliding : MonoBehaviour
                     }
                 }
             }
-            
-            
+
+
         }
 
         else
         {
             //no more sliding
             isSliding = false;
+            gun.SetBool("sliding", false);
 
             slidingSpeed = slidingBeginSpeed;
             GetComponent<PlayerMovement>().speed = GetComponent<PlayerMovement>().beginningSpeed;
             //cam position (normal walking)
             cam.position = camPosition.position;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            gun.Play("SlidingBack", 0);
         }
     }
 
@@ -108,4 +118,5 @@ public class PlayerSliding : MonoBehaviour
         //speed value needs to stay positive
         Mathf.Clamp(GetComponent<PlayerMovement>().speed, 0, GetComponent<PlayerMovement>().beginningSpeed);
     }
+
 }
