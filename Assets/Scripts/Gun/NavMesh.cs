a29eafffff67de05be3370112f729909a2c028ea
LusseveldNiek <97088465@st.deltion.nl>
2023-11-03 15:49:07 +0100

    navMesh bot

---
+++ Assets/Scripts/Gun/NavMesh.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMesh : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform[] positions;
+    public bool[] bools;
+
+    private void Start()
+    {
+        bools[0] = true;
+    }
+
+    void Update()
+    {
+        if(bools[0])
+        {
+            agent.destination = positions[0].position;
+            if(Vector3.Distance(transform.position, positions[0].position) < 1)
+            {
+                bools[0] = false;
+                bools[1] = true;
+            }
+        }
+
+        if(bools[1])
+        {
+            agent.destination = positions[1].position;
+            if (Vector3.Distance(transform.position, positions[1].position) < 1)
+            {
+                bools[1] = false;
+                bools[2] = true;
+            }
+        }
+
+        if(bools[2])
+        {
+            agent.destination = positions[2].position;
+            if (Vector3.Distance(transform.position, positions[2].position) < 1)
+            {
+                bools[2] = false;
+                bools[0] = true;
+            }
+        }
+    }
+}
